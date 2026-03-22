47c18560bb265e5d261dee112dd2c4f2a6d4f726
SachiniGayathma <slholidays2018@gmail.com>
2026-03-22 16:54:13 +0530

    The automatically deployed URLs have been added to the frontend.

--- frontend/src/components/PayButton.jsx
+++ frontend/src/components/PayButton.jsx
@@ -13,7 +13,7 @@ export default function PayButton() {
     try {
       setLoading(true);
       const response = await axios.post(
-        "https://payment-service-app.agreeableocean-2c4c42d0.southeastasia.azurecontainerapps.io/api/payment/create-session",
+        "https://payment-service-app.jollyforest-5b37db64.southeastasia.azurecontainerapps.io/api/payment/create-session",
         { amount, userId } // send userId to backend
       );
       window.location.href = response.data.url;
